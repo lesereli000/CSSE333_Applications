b198e4aee3aa10aa2c2fb2887b66344121d7ff9f
rhit-zhouy21 <83607751+rhit-zhouy21@users.noreply.github.com>
2024-01-18 17:39:51 -0500

    insert data

---
+++ src/main/InsertExternalData.java
@@ -0,0 +1,65 @@
+package main;
+
+
+public class InsertExternalData {
+	
+	public InsertExternalData() {
+		
+	}
+	
+	public boolean insertEvent() {
+		return false;
+	}
+	
+	public boolean insertGear() {
+		return false;
+	}
+	
+	public boolean insertGearType() {
+		return false;
+	}
+	
+	public boolean insertHas() {
+		return false;
+	}
+	
+	public boolean insertHeld() {
+		return false;
+	}
+	public boolean insertMatch() {
+		return false;
+	}
+	public boolean insertMatchOrganization() {
+		return false;
+	}
+	public boolean insertParticipateIn() {
+		return false;
+	}
+	public boolean insertPlacedIn() {
+		return false;
+	}
+	public boolean insertPlayedOn() {
+		return false;
+	}
+	public boolean insertPlayer() {
+		return false;
+	}
+	public boolean insertPlayerRole() {
+		return false;
+	}
+	public boolean insertPlaysFor() {
+		return false;
+	}
+	public boolean insertRole() {
+		return false;
+	}
+	public boolean insertTeam() {
+		return false;
+	}
+	public boolean insertType() {
+		return false;
+	}
+	public boolean insertUses() {
+		return false;
+	}
+}
