e9b7fccfc11528928d8199862de02ca9c8969ec6
RHIT-David <90016670+RHITDavid@users.noreply.github.com>
2024-01-18 16:56:33 -0500

    Added connection in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,41 @@
 package main;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url = "jdbc:sqlserver://${dbServer};databaseName=${dbName};"
+				+ "user=${user};password={${pass}};encrypt=false";
+		
+		String fullUrl = url.replace("${dbServer}", "golem.csse.rose-hulman.edu")
+							.replace("${dbName}", "FoodDeliverysongz1")
+							.replace("${user}", "FoodDeliveryUsersongz1")
+							.replace("${pass}", "Password123");
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(fullUrl);
+			
+			}catch (SQLException e) {
+				e.printStackTrace();
+						
+			}
+			finally {
+				
+				try {
+					if(connection != null && connection.isClosed()) connection.close();
+				
+				}catch (SQLException e) {
+					
+				}
+				
+			}
 
 	}
 
