690bcb5a6abe7e31871e9f337f7768abcf931406
lesereli000 <43320655+lesereli000@users.noreply.github.com>
2024-02-02 03:18:20 -0500

    delete is done except for minor issues

--- src/main/Delete.java
+++ src/main/Delete.java
@@ -4,11 +4,9 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Types;
-import java.util.Set;
 
 public class Delete {
 	private Connect connect;
-	private DataProcessor dp;
 	
 	public Delete(Connect connect) {
 		this.connect = connect;
@@ -20,6 +18,8 @@ public class Delete {
 			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, event);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
@@ -42,6 +42,8 @@ public class Delete {
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, player);
 			
+			stmt.execute();
+			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
 				System.out.println("Succesful delete");
@@ -62,6 +64,8 @@ public class Delete {
 			CallableStatement stmt = con.prepareCall("{? = call DeleteTeam(?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, team);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
@@ -84,6 +88,8 @@ public class Delete {
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, gear);
 			
+			stmt.execute();
+			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
 				System.out.println("Succesfull delete");
@@ -98,12 +104,14 @@ public class Delete {
 		connect.close();
 	}
 	
-	public void deleteMatch(String ID) {
+	public void deleteMatch(String match) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeleteMatch(?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
-			stmt.setString(2, ID);
+			stmt.setString(2, match);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
@@ -123,19 +131,22 @@ public class Delete {
 
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeleteHeld(?, ?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, event);
+			stmt.setString(3, org);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The Held does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deleteHeld");
 		}
 		
 		connect.close();
@@ -144,61 +155,69 @@ public class Delete {
 	public void deleteHas(String event, String match) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeleteHas(?, ?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, event);
+			stmt.setString(3, match);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The Has does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deleteHas");
 		}
 		
 		connect.close();
 	}
 
-	public void deleteOrg(String ID) {
+	public void deleteOrg(String org) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeleteMatchOrganization(?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
-			stmt.setString(2, ID);
+			stmt.setString(2, org);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The Org does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deleteOrg");
 		}
 		
 		connect.close();
 	}
 	
-	public void deleteParticipateIn(String player, String event) {
+	public void deleteParticipateIn(String player, String match) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeleteParticipateIn(?, ?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, player);
+			stmt.setString(3, match);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The record does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deleteParticipateIn");
 		}
 		
 		connect.close();
@@ -207,40 +226,46 @@ public class Delete {
 	public void deletePlacedIn(String team, String event) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeletePlacedIn(?, ?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, team);
+			stmt.setString(3, event);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The record does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deletePlacedIn");
 		}
 		
 		connect.close();
 	}
 	
-	public void deletePlayedOn(String player, String match) {
+	public void deletePlayedOn(String team, String match) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeletePlayedOn(?, ?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
-			stmt.setString(2, player);
+			stmt.setString(2, team);
+			stmt.setString(3, match);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The record does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deletePlayedOn");
 		}
 		
 		connect.close();
@@ -249,40 +274,46 @@ public class Delete {
 	public void deletePlaysFor(String player, String team) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeletePlaysFor(?, ?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
 			stmt.setString(2, player);
+			stmt.setString(3, team);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The record does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deletePlaysFor");
 		}
 		
 		connect.close();
 	}
 	
-	public void deleteUses(String Player, String Gear) {
+	public void deleteUses(String player, String Gear) {
 		Connection con = connect.getConnection();
 		try {
-			CallableStatement stmt = con.prepareCall("{? = call DeleteEvent(?)}");
+			CallableStatement stmt = con.prepareCall("{? = call DeleteUses(?, ?)}");
 			stmt.registerOutParameter(1, Types.INTEGER);
-			stmt.setString(2, Player);
+			stmt.setString(2, player);
+			stmt.setString(3,  Gear);
+			
+			stmt.execute();
 			
 			int retCode = stmt.getInt(1);
 			if (retCode == 0){
-				System.out.println("Succesfull delete");
+				System.out.println("Succesful delete");
 			} else {
-				System.out.println("The Event does not exist");
+				System.out.println("The record does not exist");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Call Failed for deleteEvent");
+			System.err.println("Call Failed for deleteUses");
 		}
 		
 		connect.close();
