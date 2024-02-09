c9bc8d518779d39dddf5489de73e87308b621283
lesereli000 <43320655+lesereli000@users.noreply.github.com>
2024-02-09 16:24:25 -0500

    merge conflict

--- src/main/UI.java
+++ src/main/UI.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -168,6 +169,7 @@ public class UI {
 		addPDpanel.add(resultLabel);
 		
 		JPanel contentPanel = new JPanel();
+		readTable(cb.getSelectedIndex(), contentPanel);
 		frame.add(contentPanel, BorderLayout.CENTER);
 		
 	    OKbtn.addActionListener(new ActionListener() {
@@ -300,7 +302,134 @@ public class UI {
 		}
 		
 		if(dataTable == null) {
+			// TODO: Display no data message
+			Object[] columnNames = {};
+			Object[][] data = {};
+			dataTable = new JTable(data, columnNames);
+		}
+		
+		dataTable.setEnabled(false);
+		contentPanel.add(dataTable);
+	}
+
+	private void deleteTable(int selectedItem, JPanel contentPanel) {
+		
+		Select select = new Select(connect);
+		JTable dataTable = null;
+		
+		switch(selectedItem) {
+		
+			case 0: {
+				//Team
+				Object[] columnNames = {"ID", "Team Name", "Sponsor", "Date Founded"};
+				Object[][] data = select.selectTeam("", "", "", "");
+				if(data == null) {
+					break;
+				}
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
 			
+			case 1: {
+				//Player
+				Object[] columnNames = {"", "", "", "", "", "", ""};
+				Object[][] data = select.selectPlayer("", "", "", "", "", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 2: {
+				//Gear
+				Object[] columnNames = {"", "", "", "", ""};
+				Object[][] data = select.selectGear("", "", "", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 3: {
+				//Match
+				Object[] columnNames = {"", "", "", ""};
+				Object[][] data = select.selectMatch("", "", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 4: {
+				//Org
+				Object[] columnNames = {"", "", "", ""};
+				Object[][] data = select.selectMatchOrganization("", "", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 5: {
+				//Event
+				Object[] columnNames = {"", "", "", "", ""};
+				Object[][] data = select.selectEvent("", "", "", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 6: {
+				//Uses
+				// TODO: make table scrollable
+				Object[] columnNames = {"Player", "Gear", "Since"};
+				Object[][] data = select.selectUses("", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 7: {
+				//Has
+				Object[] columnNames = {"", ""};
+				Object[][] data = select.selectHas("", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 8: {
+				//Held
+				Object[] columnNames = {"", ""};
+				Object[][] data = select.selectHeld("", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 9: {
+				//ParticipatesIn
+				Object[] columnNames = {"", "", ""};
+				Object[][] data = select.selectParticipateIn("", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 10: {
+				//PlacedIn
+				Object[] columnNames = {"", "", ""};
+				Object[][] data = select.selectPlacedIn("", "", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 11: {
+				//PlayedOn
+				Object[] columnNames = {"", ""};
+				Object[][] data = select.selectPlayedOn("", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+			
+			case 12: {
+				//PlaysFor
+				Object[] columnNames = {"", ""};
+				Object[][] data = select.selectPlaysFor("", "");
+				dataTable = new JTable(data, columnNames);
+				break;
+			}
+		
+		}
+		
+		if(dataTable == null) {
 			// TODO: Display no data message
 			Object[] columnNames = {};
 			Object[][] data = {};
@@ -380,231 +509,231 @@ public class UI {
         cb.setVisible(true);
 	    addPDpanel.add(cb);
 	    
-//	    JPanel inputPanel = new JPanel();
-//	    inputPanel.setMaximumSize(new Dimension(500, 800));
-//	    inputPanel.setLayout(new BorderLayout());
-//	    inputPanel.setVisible(false);
-//		
-//		JPanel infoPanel1 = new JPanel();
-//		infoPanel1.setLayout(new GridBagLayout());
-//		JLabel infoLabel1 = new JLabel("Team Name: ");
-//		infoPanel1.add(infoLabel1);
-//
-//		JTextField info1 = new JTextField();
-//		info1.setPreferredSize(new Dimension(500, 25));
-//		infoPanel1.add(info1);
-//
-//		inputPanel.add(infoPanel1, BorderLayout.NORTH);
-//	    
-//		JPanel infoPanel2 = new JPanel();
-//		infoPanel2.setLayout(new GridBagLayout());
-//		JLabel infoLabel2 = new JLabel("Team Name: ");
-//		infoPanel2.add(infoLabel2);
-//
-//		JTextField info2 = new JTextField();
-//		info2.setPreferredSize(new Dimension(500, 25));
-//		infoPanel2.add(info2);
-//
-//		infoPanel2.setVisible(false);
-//		inputPanel.add(infoPanel2, BorderLayout.CENTER);
-//	    
-//	    JButton submit = new JButton("Submit");
-//	    submit.addActionListener(new ActionListener(){
-//
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				switch(cb.getSelectedIndex()) {
-//				
-//					case 0: {
-//						//Team
-//						d.deleteTeam(info1.getText());
-//						break;
-//					}
-//					
-//					case 1: {
-//						//Player
-//						d.deletePlayer(info1.getText());
-//						break;
-//					}
-//					
-//					case 2: {
-//						//Gear
-//						d.deleteGear(info1.getText());
-//						break;
-//					}
-//					
-//					case 3: {
-//						//Match
-//						d.deleteMatch(info1.getText());
-//						break;
-//					}
-//					
-//					case 4: {
-//						//Org
-//						d.deleteOrg(info1.getText());
-//						break;
-//					}
-//					
-//					case 5: {
-//						//Event
-//						d.deleteEvent(info1.getText());
-//						break;
-//					}
-//					
-//					case 6: {
-//						//Uses
-//						d.deleteUses(info1.getText(), info2.getText());
-//						break;
-//					}
-//					
-//					case 7: {
-//						//Has
-//						d.deleteHas(info1.getText(), info2.getText());
-//						break;
-//					}
-//					
-//					case 8: {
-//						//Held
-//						d.deleteHeld(info1.getText(), info2.getText());
-//						break;
-//					}
-//					
-//					case 9: {
-//						//ParticipatesIn
-//						d.deleteParticipateIn(info1.getText(), info2.getText());
-//						break;
-//					}
-//					
-//					case 10: {
-//						//PlacedIn
-//						d.deletePlacedIn(info1.getText(), info2.getText());
-//						break;
-//					}
-//					
-//					case 11: {
-//						//PlayedIn
-//						d.deletePlayedOn(info1.getText(), info2.getText());
-//						break;
-//					}
-//					
-//					case 12: {
-//						//PlaysFor
-//						d.deletePlaysFor(info1.getText(), info2.getText());
-//						break;
-//					}
-//				
-//				}
-//			}
-//	    });
-//	    
-//	    inputPanel.add(submit, BorderLayout.SOUTH);
-//	    frame.add(inputPanel, BorderLayout.CENTER);
-//	    
-//	    OKbtn = new JButton("OK");
-//	    OKbtn.addActionListener(new ActionListener(){
-//
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				switch(cb.getSelectedIndex()) {
-//				
-//					case 0: {
-//						//Team
-//						infoLabel1.setText("Team Name: ");
-//						break;
-//					}
-//					
-//					case 1: {
-//						//Player
-//						infoLabel1.setText("Player Name: ");
-//						break;
-//					}
-//					
-//					case 2: {
-//						//Gear
-//						infoLabel1.setText("Gear Model: ");
-//						break;
-//					}
-//					
-//					case 3: {
-//						//Match
-//						infoLabel1.setText("Match Time: ");
-//						break;
-//					}
-//					
-//					case 4: {
-//						//Org
-//						infoLabel1.setText("Org Name: ");
-//						break;
-//					}
-//					
-//					case 5: {
-//						//Event
-//						infoLabel1.setText("Event Name: ");
-//						break;
-//					}
-//					
-//					case 6: {
-//						//Uses
-//						infoLabel1.setText("Player Name: ");
-//						infoLabel2.setText("Gear Model: ");
-//						infoPanel2.setVisible(true);
-//						break;
-//					}
-//					
-//					case 7: {
-//						//Has
-//						infoLabel1.setText("Event Name: ");
-//						infoLabel2.setText("Match Time: ");
-//						infoPanel2.setVisible(true);
-//						break;
-//					}
-//					
-//					case 8: {
-//						//Held
-//						infoLabel1.setText("Event Name: ");
-//						infoLabel2.setText("Org Name: ");
-//						infoPanel2.setVisible(true);
-//						break;
-//					}
-//					
-//					case 9: {
-//						//ParticipatesIn
-//						infoLabel1.setText("Player Name: ");
-//						infoLabel2.setText("Event Name: ");
-//						infoPanel2.setVisible(true);
-//						break;
-//					}
-//					
-//					case 10: {
-//						//PlacedIn
-//						infoLabel1.setText("Team Name: ");
-//						infoLabel2.setText("Event Name: ");
-//						infoPanel2.setVisible(true);
-//						break;
-//					}
-//					
-//					case 11: {
-//						//PlayedIn
-//						infoLabel1.setText("Player Name: ");
-//						infoLabel2.setText("Match Time: ");
-//						infoPanel2.setVisible(true);
-//						break;
-//					}
-//					
-//					case 12: {
-//						//PlaysFor
-//						infoLabel1.setText("Player Name: ");
-//						infoLabel2.setText("Team Name: ");
-//						infoPanel2.setVisible(true);
-//						break;
-//					}
-//				
-//				}
-//				inputPanel.setVisible(true);
-//			}
-//	    	
-//	    });
-//	    addPDpanel.add(OKbtn);
+	    JPanel inputPanel = new JPanel();
+	    inputPanel.setMaximumSize(new Dimension(500, 800));
+	    inputPanel.setLayout(new BorderLayout());
+	    inputPanel.setVisible(false);
+		
+		JPanel infoPanel1 = new JPanel();
+		infoPanel1.setLayout(new GridBagLayout());
+		JLabel infoLabel1 = new JLabel("Team Name: ");
+		infoPanel1.add(infoLabel1);
+
+		JTextField info1 = new JTextField();
+		info1.setPreferredSize(new Dimension(500, 25));
+		infoPanel1.add(info1);
+
+		inputPanel.add(infoPanel1, BorderLayout.NORTH);
+	    
+		JPanel infoPanel2 = new JPanel();
+		infoPanel2.setLayout(new GridBagLayout());
+		JLabel infoLabel2 = new JLabel("Team Name: ");
+		infoPanel2.add(infoLabel2);
+
+		JTextField info2 = new JTextField();
+		info2.setPreferredSize(new Dimension(500, 25));
+		infoPanel2.add(info2);
+
+		infoPanel2.setVisible(false);
+		inputPanel.add(infoPanel2, BorderLayout.CENTER);
+	    
+	    JButton submit = new JButton("Submit");
+	    submit.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				switch(cb.getSelectedIndex()) {
+				
+					case 0: {
+						//Team
+						d.deleteTeam(info1.getText());
+						break;
+					}
+					
+					case 1: {
+						//Player
+						d.deletePlayer(info1.getText());
+						break;
+					}
+					
+					case 2: {
+						//Gear
+						d.deleteGear(info1.getText());
+						break;
+					}
+					
+					case 3: {
+						//Match
+						d.deleteMatch(info1.getText());
+						break;
+					}
+					
+					case 4: {
+						//Org
+						d.deleteOrg(info1.getText());
+						break;
+					}
+					
+					case 5: {
+						//Event
+						d.deleteEvent(info1.getText());
+						break;
+					}
+					
+					case 6: {
+						//Uses
+						d.deleteUses(info1.getText(), info2.getText());
+						break;
+					}
+					
+					case 7: {
+						//Has
+						d.deleteHas(info1.getText(), info2.getText());
+						break;
+					}
+					
+					case 8: {
+						//Held
+						d.deleteHeld(info1.getText(), info2.getText());
+						break;
+					}
+					
+					case 9: {
+						//ParticipatesIn
+						d.deleteParticipateIn(info1.getText(), info2.getText());
+						break;
+					}
+					
+					case 10: {
+						//PlacedIn
+						d.deletePlacedIn(info1.getText(), info2.getText());
+						break;
+					}
+					
+					case 11: {
+						//PlayedIn
+						d.deletePlayedOn(info1.getText(), info2.getText());
+						break;
+					}
+					
+					case 12: {
+						//PlaysFor
+						d.deletePlaysFor(info1.getText(), info2.getText());
+						break;
+					}
+				
+				}
+			}
+	    });
+	    
+	    inputPanel.add(submit, BorderLayout.SOUTH);
+	    frame.add(inputPanel, BorderLayout.CENTER);
+	    
+	    OKbtn = new JButton("OK");
+	    OKbtn.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				switch(cb.getSelectedIndex()) {
+				
+					case 0: {
+						//Team
+						infoLabel1.setText("Team Name: ");
+						break;
+					}
+					
+					case 1: {
+						//Player
+						infoLabel1.setText("Player Name: ");
+						break;
+					}
+					
+					case 2: {
+						//Gear
+						infoLabel1.setText("Gear Model: ");
+						break;
+					}
+					
+					case 3: {
+						//Match
+						infoLabel1.setText("Match Time: ");
+						break;
+					}
+					
+					case 4: {
+						//Org
+						infoLabel1.setText("Org Name: ");
+						break;
+					}
+					
+					case 5: {
+						//Event
+						infoLabel1.setText("Event Name: ");
+						break;
+					}
+					
+					case 6: {
+						//Uses
+						infoLabel1.setText("Player Name: ");
+						infoLabel2.setText("Gear Model: ");
+						infoPanel2.setVisible(true);
+						break;
+					}
+					
+					case 7: {
+						//Has
+						infoLabel1.setText("Event Name: ");
+						infoLabel2.setText("Match Time: ");
+						infoPanel2.setVisible(true);
+						break;
+					}
+					
+					case 8: {
+						//Held
+						infoLabel1.setText("Event Name: ");
+						infoLabel2.setText("Org Name: ");
+						infoPanel2.setVisible(true);
+						break;
+					}
+					
+					case 9: {
+						//ParticipatesIn
+						infoLabel1.setText("Player Name: ");
+						infoLabel2.setText("Event Name: ");
+						infoPanel2.setVisible(true);
+						break;
+					}
+					
+					case 10: {
+						//PlacedIn
+						infoLabel1.setText("Team Name: ");
+						infoLabel2.setText("Event Name: ");
+						infoPanel2.setVisible(true);
+						break;
+					}
+					
+					case 11: {
+						//PlayedIn
+						infoLabel1.setText("Player Name: ");
+						infoLabel2.setText("Match Time: ");
+						infoPanel2.setVisible(true);
+						break;
+					}
+					
+					case 12: {
+						//PlaysFor
+						infoLabel1.setText("Player Name: ");
+						infoLabel2.setText("Team Name: ");
+						infoPanel2.setVisible(true);
+						break;
+					}
+				
+				}
+				inputPanel.setVisible(true);
+			}
+	    	
+	    });
+	    addPDpanel.add(OKbtn);
 
 	    frame.pack();
 		frame.setVisible(true);
